2267f38e99695524e89e36801162fae67f8aa067
Yuri Lee <yurilee.tech@gmail.com>
2022-03-29 21:21:31 +0900

    📰 Feat: 사용자 목록 컴포넌트 추가

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -10,11 +10,9 @@ const AddUser = () => {
 
   const addUserHandler = (event) => {
     event.preventDefault();
-
     if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
       return;
     }
-
     if (+enteredAge < 1) {
       return;
     }
