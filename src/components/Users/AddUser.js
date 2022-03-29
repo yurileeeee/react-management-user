725595bff721642e57cfe510fa121d1cf1ba341f
Yuri Lee <yurilee.tech@gmail.com>
2022-03-29 21:14:36 +0900

    📰 Feat: 입력값 유효성 검사

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -10,6 +10,17 @@ const AddUser = () => {
 
   const addUserHandler = (event) => {
     event.preventDefault();
+
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      return;
+    }
+
+    if (+enteredAge < 1) {
+      return;
+    }
+
+    setEnteredUsername('');
+    setEnteredAge('');
   };
 
   const usernameChangeHandler = (event) => {
@@ -24,9 +35,19 @@ const AddUser = () => {
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor='username'>Username</label>
-        <input id='username' type='text' onChange={usernameChangeHandler} />
+        <input
+          id='username'
+          type='text'
+          value={enteredUsername}
+          onChange={usernameChangeHandler}
+        />
         <label htmlFor='age'>Age (Years)</label>
-        <input id='age' type='number' onChange={ageChangeHandler} />
+        <input
+          id='age'
+          type='number'
+          value={enteredAge}
+          onChange={ageChangeHandler}
+        />
         <Button type='submit'>Add User</Button>
       </form>
     </Card>
