3b1beed012665c4da420e23f50898279f6424ee1
Yuri Lee <yurilee.tech@gmail.com>
2022-03-30 20:47:09 +0900

    📰 Feat: createPortal 로 렌더링되는 위치 옮겨주기

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,25 +1,47 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 import Card from './Card';
 import Button from './Button';
 import classes from './ErrorModal.module.css';
 
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} onClick={props.onConfirm} />;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <Card className={classes.modal}>
+      <header className={classes.header}>
+        <h2>{props.title}</h2>
+      </header>
+      <div className={classes.content}>
+        <p>{props.message}</p>
+      </div>
+      <footer className={classes.actions}>
+        <Button onClick={props.onConfirm}>Okay</Button>
+      </footer>
+    </Card>
+  );
+};
+
 const ErrorModal = (props) => {
   return (
-    <div>
-      <div className={classes.backdrop} onClick={props.onConfirm} />
-      <Card className={classes.modal}>
-        <header className={classes.header}>
-          <h2>{props.title}</h2>
-        </header>
-        <div className={classes.content}>
-          <p>{props.message}</p>
-        </div>
-        <footer className={classes.actions}>
-          <Button onClick={props.onConfirm}>Okay</Button>
-        </footer>
-      </Card>
-    </div>
+    <React.Fragment>
+      {ReactDOM.createPortal(
+        // portal : 렌더링되려는 HTML 콘텐츠를 다른 위치로 옮겨줌
+        <Backdrop onConfirm={props.onConfirm} />,
+        document.getElementById('backdrop-root')
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay
+          title={props.title}
+          message={props.message}
+          onConfirm={props.onConfirm}
+        />,
+        document.getElementById('overlay-root')
+      )}
+    </React.Fragment>
   );
 };
 
